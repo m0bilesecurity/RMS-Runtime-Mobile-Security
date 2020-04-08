732a4e466b0d717e732abe5bc37a53a9361ca3d4
b1gb1t <12282235+b1gb1t@users.noreply.github.com>
2020-04-08 16:43:29 +0200

    Remove flash import

--- mobilesecurity.py
+++ mobilesecurity.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 import frida, sys, os
 import json
