00f0ebb2ac47d86c957b7d023013aa6bdb2cbd6e
Mobile Security <26333219+m0bilesecurity@users.noreply.github.com>
2020-05-10 19:52:21 +0200

    new custom scripts - env paths and native hook template

---
+++ custom_scripts/get_app_env_info.js
@@ -0,0 +1,43 @@
+/************************************************************************
+ * Name: Android App Environment
+ * OS: Android
+ * Author: @mobilesecurity_
+ * Source: https://github.com/m0bilesecurity
+ * Info: 
+    * filesDirectory
+    * cacheDirectory and externalCacheDirectory
+    * codeCacheDirectory
+    * obbDir
+    * packageCodePath
+*************************************************************************/
+
+Java.perform(function() {
+    var context = null
+    var ActivityThread = Java.use('android.app.ActivityThread');
+    var targetApp = ActivityThread.currentApplication();
+
+    if (targetApp != null) {
+        context = targetApp.getApplicationContext();
+        var env = {
+            filesDirectory: context.getFilesDir().getAbsolutePath().toString(),
+            cacheDirectory: context.getCacheDir().getAbsolutePath().toString(),
+            externalCacheDirectory: context.getExternalCacheDir().getAbsolutePath().toString(),
+            codeCacheDirectory: 
+                'getCodeCacheDir' in context ? 
+                context.getCodeCacheDir().getAbsolutePath().toString() : 'N/A',
+            obbDir: context.getObbDir().getAbsolutePath().toString(),
+            packageCodePath: context.getPackageCodePath().toString(),
+        };
+
+        send("******************* App Environment Info *******************")
+        send("filesDirectory: "+env.filesDirectory);
+        send("cacheDirectory: "+env.cacheDirectory);
+        send("externalCacheDirectory: "+env.externalCacheDirectory);
+        send("codeCacheDirectory: "+env.codeCacheDirectory);
+        send("obbDir: "+env.obbDir);
+        send("packageCodePath: "+env.packageCodePath);
+        send("************************************************************")
+
+    } else console.log("Error: App Environment Info - N/A")
+
+});
