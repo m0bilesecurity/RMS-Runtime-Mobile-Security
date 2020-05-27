f6b31b0e51a577b56f5c2838c699bff4dc8f0c30
Mobile Security <26333219+m0bilesecurity@users.noreply.github.com>
2020-05-27 14:41:11 +0200

    rename Android ID script x2

--- custom_scripts/get_ANDROID_ID.js
+++ custom_scripts/get_ANDROID_ID.js
@@ -1,10 +1,10 @@
-/************************************************************************
+/***********************************************************************
  * Name: get ANDROID_ID
  * OS: Android
  * Author: iddoeldor
  * Source: https://github.com/iddoeldor/frida-snippets#get-android-id
  * Info: The ANDROID_ID is unique in each application 
- *************************************************************************/
+ ***********************************************************************/
 
 Java.perform(function () {
   function getContext() {
