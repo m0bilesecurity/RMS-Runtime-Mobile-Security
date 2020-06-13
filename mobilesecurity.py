73705b567121e139d5babbc3f66b4c29e640d04b
Mobile Security <26333219+m0bilesecurity@users.noreply.github.com>
2020-06-14 00:21:33 +0200

    bump RMS version to 1.3.1

--- mobilesecurity.py
+++ mobilesecurity.py
@@ -1043,7 +1043,7 @@ if __name__ == '__main__':
     print("")
     print("_________________________________________________________")
     print("RMS - Runtime Mobile Security")
-    print("Version: 1.3")
+    print("Version: 1.3.1")
     print("by @mobilesecurity_")
     print("Twitter Profile: https://twitter.com/mobilesecurity_")
     print("_________________________________________________________")
