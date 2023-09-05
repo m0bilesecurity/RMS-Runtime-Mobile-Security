e4a029069b6232f2b3f467cbfd492e85f5faa171
npu <np@seq.lv>
2023-09-05 09:22:41 +0000

    Shorten mobilesecurity.py
    Use only one print function to print the message

--- mobilesecurity.py
+++ mobilesecurity.py
@@ -1,19 +1,16 @@
-print("**************************************************************************")
-print("-------------------------------ATTENTION----------------------------------")
-print("**************************************************************************")
-print("")
+print("""**************************************************************************
+-------------------------------ATTENTION----------------------------------
+**************************************************************************
 
-print("RMS-Runtime-Mobile-Security is now powered by NodeJS.")
-print("Python mode will no longer be supported.")
-print("")
-print("In order to install the new NodeJS version, launch the following commands:")
-print(" 1. npm install -g rms-runtime-mobile-security")
-print(" 2. rms (or RMS-Runtime-Mobile-Security)")
+RMS-Runtime-Mobile-Security is now powered by NodeJS.
+Python mode will no longer be supported.
 
-print("")
-print("More info here:")
-print("--> https://github.com/m0bilesecurity/RMS-Runtime-Mobile-Security")
-print("")
+In order to install the new NodeJS version, launch the following commands:
+ 1. npm install -g rms-runtime-mobile-security
+ 2. rms (or RMS-Runtime-Mobile-Security)
 
-print("**************************************************************************")
-print("**************************************************************************")
+More info here:
+--> https://github.com/m0bilesecurity/RMS-Runtime-Mobile-Security
+
+**************************************************************************
+**************************************************************************""")
